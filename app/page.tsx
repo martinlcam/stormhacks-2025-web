8c643900044b7d00c916fb9d348c42862c381bc5
Martin <martinlucam@gmail.com>
2025-10-04 13:23:35 -0700

    patch

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+
+    <main className="h-screen w-screen overflow-hidden">
+      <div className="flex text-[150px] justify-center items-center h-full w-full">hello ilia</div>
+    </main>
+
+  )
+}
