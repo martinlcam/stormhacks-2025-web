063f8c03f0a275bf0264fdcbcbeb56e30d7a0154
Martin <martinlucam@gmail.com>
2025-10-04 15:01:39 -0700

    homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ export default function Home() {
   return (
 
     <main className="h-screen w-screen overflow-hidden">
-      <div className="flex text-[150px] justify-center items-center h-full w-full">hello ilia</div>
-      <div className="flex justify-center">
-        <a href="/stream-preview" className="text-blue-500 underline text-2xl">
-        Stream Preview
-      </a>
-      </div>
+      <div className="flex text-[150px] justify-center items-center h-full w-full">
+        <a href="/stream-preview" className="text-[50px]">
+          Stream Preview
+        </a></div>
+
+
+
     </main>
   )
 }
