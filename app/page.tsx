f42a3551e119547e4737796504c15fcfa451b48e
Martin <martinlucam@gmail.com>
2025-10-04 16:19:32 -0700

    restarting vs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-
-
 export default function Home() {
   return (
+
+    
 
     <main className="h-screen w-screen overflow-hidden">
       <div className="flex text-[150px] justify-center items-center h-full w-full">
