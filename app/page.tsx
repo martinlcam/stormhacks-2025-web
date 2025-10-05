f1edfa439ab13562b53dc69d51789d40128baa0a
Martin <martinlucam@gmail.com>
2025-10-04 20:45:35 -0700

    landing page updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,26 @@ export default function Home() {
       <main className="min-h-screen w-screen flex flex-col overflow-auto">
         <Header/>
 
-        <div className="flex flex-col items-center flex-grow justify-start px-4 md:px-8">
+        <div className="flex flex-col items-center justify-start px-4 md:px-8">
 
-          <div className="w-full max-w-200 mt-[10vh] md:mt-[20vh]">
+          <div className="w-full max-w-200 mt-[15vh] md:mt-[20vh]">
             <div className="text-5xl font-semibold text-center mb-8">
-              The solution for the vision impaired
+              An all in one solution for the auditorily impaired
             </div>
           </div>
 
           <div className="w-full max-w-200 flex justify-center">
             <Link href="/stream-preview">
               <button className="text-[20px] px-8 py-4 bg-[#0052ff] text-white rounded-4xl shadow-xl hover:bg-[#47618a] transition" type="button">
-                Try for free!
+                Try for Free!
               </button>
             </Link>
-
           </div>
         </div>
+
+        {/*jesse im blue*/}
+        <div className="w-full mt-10 bg-blue-600 h-150"/>
+
       </main>
   )
 }
--- app/stream-preview/page.tsx
+++ app/stream-preview/page.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 export default function Page() {
   return (
-    <main className="p-4">
+    <main className="">
       <div className="flex text-[70px] mt-20 justify-center">Live Stream Preview</div>
       <Header/>
       <StreamPreview/>
